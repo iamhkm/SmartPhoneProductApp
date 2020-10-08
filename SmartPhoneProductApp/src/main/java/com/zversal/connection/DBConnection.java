dcf7ca1cb46a19852087f96148a982048de6d8ad
iamhkm <himanshumittal2014@gmail.com>
2020-10-08 14:52:40 +0530

    update 08/10/2020 02:42
    some changes

--- SmartPhoneProductApp/src/main/java/com/zversal/connection/DBConnection.java
+++ SmartPhoneProductApp/src/main/java/com/zversal/connection/DBConnection.java
@@ -1,20 +1,36 @@
 package com.zversal.connection;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 public class DBConnection {
 
-	private final String configFile;
-	private final HikariConfig cfg;
-	private final HikariDataSource ds;
-
+	//private final String configFile;
+	private HikariConfig cfg = null;
+	private HikariDataSource ds = null;
+	private Properties properties = new Properties();
+	
+	  /*
+	  public DBConnection() 
+	  { configFile ="res/DBConnectionAttributes.properties";
+	  cfg = new HikariConfig(configFile); 
+	  ds = new HikariDataSource(cfg);
+	  }
+      */
+	
 	public DBConnection() {
-		configFile = "src/main/res/DBConnectionAttributes.properties";
-		cfg = new HikariConfig(configFile);
-		ds = new HikariDataSource(cfg);
+		try (InputStream iStream = this.getClass().getResourceAsStream("/DBConnectionAttributes.properties");) {
+			properties.load(iStream);
+			cfg = new HikariConfig(properties);
+			ds = new HikariDataSource(cfg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public Connection connection() {
--- SmartPhoneProductApp/src/main/java/com/zversal/test/CheckNumberOfProductsInTable.java
+++ SmartPhoneProductApp/src/main/java/com/zversal/test/CheckNumberOfProductsInTable.java
@@ -6,23 +6,47 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 class CheckNumberOfProductsInTable extends Thread {
+	
+	volatile boolean finished = false;
+
+	  public void stopMe()
+	  {
+	    finished = true;
+	  }
+	
 	public void run() {
+		while (!finished)
+	    {
 		for (;;) {
+			if(!finished){
 			try (Connection con = dbconnection.connection();
 					PreparedStatement ps = con.prepareStatement("select count(*) from products");
 					ResultSet rs = ps.executeQuery();) {
 				rs.next();
 				int count = rs.getInt(1);
 				System.out.println(count);
-				Thread.sleep(5000);
+				Thread.sleep(1000);
 			} catch (Exception e) {
 				System.out.println(e);
 			}
 		}
+			else{
+				break;
+			}
+		}
+	    }
 	}
 
 	public static void main(String args[]) {
 		CheckNumberOfProductsInTable t1 = new CheckNumberOfProductsInTable();
 		t1.start();
+		try {
+			CheckNumberOfProductsInTable.sleep(10000);
+			t1.stopMe();
+			System.out.println(t1.getName()+"stopped");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
